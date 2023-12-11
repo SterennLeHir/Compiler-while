07e00c49d4188e41c8d80b0c46bd396bf22c2a57
TheKingHydra <95933117+TheKingHydra@users.noreply.github.com>
2023-12-11 15:55:01 +0100

    Created test.txt Modified Main.java

--- src/main/java/fr/esir/tlc/Main.java
+++ src/main/java/fr/esir/tlc/Main.java
@@ -1,9 +1,22 @@
 package fr.esir.tlc;
 
-import java.util.Stack;
+import org.antlr.runtime.ANTLRFileStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        whileLexer lex = new whileLexer(new ANTLRFileStream("C:\\Users\\hywel\\OneDrive\\Documents\\BOULOT\\M1\\S1\\TLC\\Projet\\Compiler-while\\output\\__Test___input.txt", "UTF8"));
+        CommonTokenStream tokens = new CommonTokenStream(lex);
 
+        whileParser g = new whileParser(tokens, null);
+        try {
+            g.program();
+            System.out.println("j'essaie");
+        } catch (RecognitionException e) {
+            e.printStackTrace();
+        }
+        System.out.println("j'ai fini");
+        System.out.println(g.program().tree.toString());
     }
 }
