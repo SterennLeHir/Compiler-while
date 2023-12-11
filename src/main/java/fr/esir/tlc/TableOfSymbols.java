49f4724f919d3808a50f61215bd5d1e3070dd977
TheKingHydra <95933117+TheKingHydra@users.noreply.github.com>
2023-12-11 14:18:21 +0100

    Updated TableOfSymbols

--- src/main/java/fr/esir/tlc/TableOfSymbols.java
+++ src/main/java/fr/esir/tlc/TableOfSymbols.java
@@ -3,14 +3,13 @@ package fr.esir.tlc;
 import java.util.HashSet;
 import java.util.Set;
 
-
 /*
 On crée une nouvelle table à chaque truc.
 Une table pour les fonctions.
-Une table pour les paramètres.
+Une table pour les paramètres. (À voir, parce qu'en soient les paramètres sont une forme de variables locales)
 Une table pour les variables locales.
 
-Faut faire un pattern visitor sur l'AST pour générer la table des symboles
+Il faut faire un modèle visitor sur l'AST pour générer la table des symboles
 Donc, il faut que la table ait un type, un nom (la fonction pour les paramètres/variables locales ; le programme pour les fonctions)
 
 Il faut que la table stocke soit :
@@ -24,26 +23,31 @@ Si type = "fonction" : sauver aussi la taille entrée et sortie.
 
 public class TableOfSymbols {
     private final String name; //refers to the parent function or program
-    private final String type; //Refers to what type of table it is (functions/parameters/variables)
-    private Set<String> set;
+    private final Set<String> set;
     private final int entries;
     private final int outputs;
+
     /**
      * This is the constructor.
-     * @param name The name of the symbol
+     * @param name The name of the parent class/
      */
-    public TableOfSymbols(String name, String type){
+    public TableOfSymbols(String name){
         this.name = name;
-        this.type = type;
         this.set = new HashSet<>();
         this.entries=0;
         this.outputs=0;
     }
 
-    public TableOfSymbols(String name, String type, int entries, int outputs){
+    /**
+     * This is the constructor for a Table affiliated with a parent function.
+     * @param name The name of the parent function.
+     * @param entries The number of parameters.
+     * @param outputs The number of outputs.
+     * @param parameters The parameters of the function.
+     */
+    public TableOfSymbols(String name, int entries, int outputs, Set<String> parameters){
         this.name = name;
-        this.type = type;
-        this.set = new HashSet<>();
+        this.set = parameters;
         this.entries = entries;
         this.outputs = outputs;
     }
@@ -62,15 +66,23 @@ public class TableOfSymbols {
      * @return The toString version of the table
      */
     public String toString (){
-        return this.name+" ("+type+") : ";
+        return this.name+" ("+entries+","+outputs+") : "+ set.toString();
     }
 
     /**
      * This function returns true if the key is present in the set.
-     * @param key : a key to find
+     * @param variable : a variable to find in the table
      * @return true if present, false if not
      */
-    public boolean foundKey (String key){
-        return this.set.contains(key);
+    public boolean foundVariable (String variable){
+        return this.set.contains(variable);
+    }
+
+    /**
+     * This function adds the variable to the set, if the variable isn't there.
+     * @param variable The variable to add to the table
+     */
+    public void addVariableToSet(String variable){
+        if (!foundVariable(variable)) this.set.add(variable);
     }
 }
